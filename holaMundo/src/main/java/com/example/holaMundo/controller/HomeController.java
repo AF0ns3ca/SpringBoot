817e4bd749893eb5a8d6b4c73257befdc32e9d3a
AF0ns3ca <afonsecahdez@gmail.com>
2025-03-15 15:08:54 +0100

    Primera aplicacion hola mundo y comienzo del portal de empleo

---
+++ holaMundo/src/main/java/com/example/holaMundo/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.holaMundo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HomeController {
+    
+    @GetMapping("/")
+    public String inicio() {
+        return "Hola Mundo que tal estamos todos";
+    }
+
+}
